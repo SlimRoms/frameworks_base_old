c910d868d1967d23de60194df6cb01e7b3510231
Ricardo Cerqueira <github@cerqueira.org>
2012-03-14 19:25:27 +0000

    bootanim: Don't cache textures if they're expected to use a lot of VRAM
    bootanimation cached all the textures generated until the animation
    was completed.
    For Hi-res animations with lots of frames (like the new CM9 anim on
    xhdpi devices), this implied using large amounts of video RAM, more
    than most devices actually have available, causing the animation to
    stall (and in some cases, gralloc to lock up); so if an animation is
    expected to use a lot of RAM (rough estimation based on frame size
    and number of frames), disable cache entirely and generate the textures
    on demand
    
    Change-Id: I157e5cdde2aab1d82d980d77cbedd9f127c83a1d
    
    Conflicts:
    
    	cmds/bootanimation/BootAnimation.cpp
    
    Conflicts:
    
    	cmds/bootanimation/BootAnimation.cpp

--- cmds/bootanimation/BootAnimation.cpp
+++ cmds/bootanimation/BootAnimation.cpp
@@ -550,7 +550,7 @@ bool BootAnimation::movie()
                 const Animation::Frame& frame(part.frames[j]);
                 nsecs_t lastFrame = systemTime();
 
-                if (r > 0) {
+                if (r > 0 && !noTextureCache) {
                     glBindTexture(GL_TEXTURE_2D, frame.tid);
                 } else {
                     if (part.count != 1) {
@@ -595,6 +595,9 @@ bool BootAnimation::movie()
                 }
 
                 checkExit();
+ 
+                if (noTextureCache)
+                    glDeleteTextures(1, &frame.tid);
             }
 
             usleep(part.pause * ns2us(frameDuration));
